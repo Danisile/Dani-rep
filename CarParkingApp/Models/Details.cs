76a678537b8348696b0c7faf336c0373b038839d
Danisile Johannah <77388022+Danisile@users.noreply.github.com>
2022-03-18 11:11:21 +0200

    Project

---
+++ CarParkingApp/Models/Details.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace CarParkingApp.Models
+{
+    public class Details
+    {
+        [Key]
+        [Display(Name ="ID")]
+        public int Person_Id { get; set; }
+        [Required]
+        [Display(Name ="Full Name")]
+        public string Person_Name { get; set; }
+
+        [Display(Name = "ID Number")]
+        public string Person_IdNum{ get; set; }
+
+        [Display(Name ="Gender")]
+        public string Person_Gender { get; set; }  
+
+        
+        
+    }
+}
---
+++ CarParkingApp/Models/Parking.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace CarParkingApp.Models
+{
+    public class Parking
+    {
+        [Key]
+        public int Parking_Num { get; set; }
+        [Required]  
+        public string Parking_Type { get; set; }
+        public DateTime Slots{ get; set; }
+
+        public string Park_Space { get; set; }
+
+        public ICollection<Payment> payments { get; set; }
+        
+
+    }
+}
